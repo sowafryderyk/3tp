9d82c065e3c1423e508a7a7855a93eea4ca54612
Fryderyk S <fryderyksow@gmail.com>
2024-10-28 10:12:53 +0100

    dodanie nowejj funkcji

--- 1to5.js
+++ 1to5.js
@@ -1,5 +1,15 @@
 let i = 0;
-let intervalId = setInterval(()=>{
-    console.log(i++);
-    if(i==6) clearInterval(intervalId);
+// let intervalId = setInterval(()=>{
+//     console.log(i++);
+//     if(i==6) clearInterval(intervalId);
+// },1000);
+
+//nowa funkcja wyswietla czas co sekunde
+function addZero(num){
+    return (num<10) ? '0' + num : num
+}
+setInterval( ()=>{
+    let data = new Date();
+    console.clear();
+    console.log(addZero(data.getHours()) +" : "+ addZero(data.getMinutes()) +" : "+ addZero(data.getSeconds()));
 },1000)
